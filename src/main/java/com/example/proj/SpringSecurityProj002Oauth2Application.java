19f3d748df41d0f3abf364a4b0e01f88ed10a67d
JavaN5 <thilinashamal19@gmail.com>
2024-11-13 03:10:23 +0530

    Initial Commit

---
+++ src/main/java/com/example/proj/SpringSecurityProj002Oauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProj002Oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityProj002Oauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proj/SpringSecurityProj002Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProj002Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
